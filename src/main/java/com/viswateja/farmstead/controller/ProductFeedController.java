4858acea99cc540526de8f5006450446919f7e50
saiviswatejasreerama <saiviswatejasreerama@outlook.com>
2021-11-17 18:40:39 +0530

    added request mapping

--- src/main/java/com/viswateja/farmstead/controller/ProductFeedController.java
+++ src/main/java/com/viswateja/farmstead/controller/ProductFeedController.java
@@ -2,13 +2,17 @@ package com.viswateja.farmstead.controller;
 
 import com.viswateja.farmstead.service.ProductFeed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class ProductFeedController {
     @Autowired
     private ProductFeed productFeed;
 
+    @GetMapping("/productfeed")
     public void runProductFeed() {
         productFeed.run();
     }
